13b1e0b2301a1d2409ed4b1f90dcaa2e65c0a114
gpallabi <190396@softwarica.edu.np>
2022-08-23 22:33:10 +0545

    first commit

---
+++ API/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "qr_implement";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+?>
